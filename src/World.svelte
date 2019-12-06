27761323e51fc92ff4eda7b7857bbe35181eadc8
Juha Wiberg <juha.wiberg@gmail.com>
2019-12-06 13:52:08 +0200

    Extracted World component.

---
+++ src/World.svelte
@@ -0,0 +1,42 @@
+<script>
+  import Cell from "./Cell.svelte";
+
+  const cols = [...Array(100).keys()];
+  const rows = [...Array(100).keys()];
+  const ids = [...Array(cols.length * rows.length).keys()];
+  let living = new Array(ids.length).fill(false);
+
+  console.log(ids);
+
+  function getIdForCell() {
+    const id = ids[0];
+    ids.shift();
+    return id;
+  }
+
+  $: {
+    console.log(living);
+  }
+
+  /*setInterval(() => {
+    console.log("running");
+  }, 1000);*/
+</script>
+
+<style>
+  .world {
+    display: grid;
+    grid-gap: 0.1em;
+    grid-template-columns: repeat(100, 1fr);
+  }
+</style>
+
+<main>
+  <div class="world">
+    {#each cols as c}
+      {#each rows as r}
+        <Cell x={c} y={r} id={getIdForCell()} bind:living />
+      {/each}
+    {/each}
+  </div>
+</main>
