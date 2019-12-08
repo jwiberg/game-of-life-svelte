3925a3f1b68ffc85f1d61778973b10159627f39a
Juha Wiberg <juha.wiberg@gmail.com>
2019-12-08 12:03:09 +0200

    Test framework now works with ES6. Also added skeleton  for GameOfLive logic.

---
+++ src/game-of-life.js
@@ -0,0 +1,5 @@
+export default class GameOfLife {
+  static test() {
+    console.log("hello world")
+  }
+}
---
+++ test/game-of-life-test.js
@@ -0,0 +1,7 @@
+import test from "ava"
+import GameOfLife from "../src/game-of-life"
+
+test("this works", t => {
+  GameOfLife.test()
+  t.pass()
+})
