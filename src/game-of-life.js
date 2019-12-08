201697aadb6bb83b662d93c5104693301bd5761f
Juha Wiberg <juha.wiberg@gmail.com>
2019-12-08 18:23:45 +0200

    Some refactorings. Code is still a little bit unclean...

--- src/game-of-life.js
+++ src/game-of-life.js
@@ -13,19 +13,28 @@ export function generateNext(previous) {
   let next = createEmptyGrid(previous[0].length, previous.length)
   for (let row = 0; row < previous.length; row++) {
     for (let col = 0; col < previous[row].length; col++) {
-      let neighbourCount = 0
-      for (let x = -1; x < 2; x += 1) {
-        if(col + x == col) {
-          continue
-        }
-        if (previous[row][col + x] === true) {
-          neighbourCount++
-        }
-      }
-      if (neighbourCount == 2) {
+      if (countNeighbours(row, col, previous) == 2) {
         next[row][col] = true
       }
     }
   }
   return next
+}
+
+function countNeighbours(row, col, previous) {
+  let neighbourCount = 0
+  for (let x = -1; x < 2; x += 1) {
+    if (!isItself(col, x) && isAliveNeighbour(previous, row, col, x)) {
+      neighbourCount++
+    }
+  }
+  return neighbourCount
+}
+
+function isAliveNeighbour(previous, row, col, x) {
+  return previous[row][col + x] === true
+}
+
+function isItself(col, x) {
+  return col + x === col
 }
