f671a418bf1e6472a56f19062432da395748a73d
Juha Wiberg <juha.wiberg@gmail.com>
2019-12-08 17:56:07 +0200

    First tests passed.

--- src/game-of-life.js
+++ src/game-of-life.js
@@ -1,5 +1,31 @@
-export default class GameOfLife {
-  static test() {
-    console.log("hello world")
+export function createEmptyGrid(worldWidth, worldHeight) {
+  let grid = []
+  for (let row = 0; row < worldHeight; row++) {
+    grid.push([])
+    for (let col = 0; col < worldWidth; col++) {
+      grid[row][col] = false
+    }
   }
+  return grid
+}
+
+export function generateNext(previous) {
+  let next = createEmptyGrid(previous[0].length, previous.length)
+  for (let row = 0; row < previous.length; row++) {
+    for (let col = 0; col < previous[row].length; col++) {
+      let neighbourCount = 0
+      for (let x = -1; x < 2; x += 1) {
+        if(col + x == col) {
+          continue
+        }
+        if (previous[row][col + x] === true) {
+          neighbourCount++
+        }
+      }
+      if (neighbourCount == 2) {
+        next[row][col] = true
+      }
+    }
+  }
+  return next
 }
--- test/game-of-life-test.js
+++ test/game-of-life-test.js
@@ -1,7 +1,109 @@
 import test from "ava"
-import GameOfLife from "../src/game-of-life"
+import * as gol from "../src/game-of-life"
 
-test("this works", t => {
-  GameOfLife.test()
-  t.pass()
+test("Returns initialized grid for starting point", t => {
+  t.deepEqual(gol.createEmptyGrid(5, 5), [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
+})
+
+test("Empty generation generates empty generation", t => {
+  t.deepEqual(gol.generateNext(gol.createEmptyGrid(5, 5)), [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
+})
+
+test("Cell without any neighbours dies", t => {
+  const generationX = [
+    [true, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ]
+
+  t.deepEqual(gol.generateNext(generationX), [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
+})
+
+test("Cells with only one neighbour dies", t => {
+  const generationX = [
+    [true, true, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ]
+
+  t.deepEqual(gol.generateNext(generationX), [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
+})
+
+test("Cells with two neighbours stays alive left corner", t => {
+  const generationX = [
+    [true, true, true, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ]
+  t.deepEqual(gol.generateNext(generationX), [
+    [false, true, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
+})
+
+test("Cells with two neighbours stays alive middle of first line", t => {
+  const generationX = [
+    [false, true, true, true, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ]
+  t.deepEqual(gol.generateNext(generationX), [
+    [false, false, true, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
+})
+
+test("Cells with two neighbours stays alive middle of the world", t => {
+  const generationX = [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, true, true, true, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ]
+  t.deepEqual(gol.generateNext(generationX), [
+    [false, false, false, false, false],
+    [false, false, false, false, false],
+    [false, false, true, false, false],
+    [false, false, false, false, false],
+    [false, false, false, false, false]
+  ])
 })
