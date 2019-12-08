85c273e8952981c8c6ceb15fb032592a552974ae
Juha Wiberg <juha.wiberg@gmail.com>
2019-12-08 23:00:35 +0200

    Still some refaktoring.

--- src/game-of-life.js
+++ src/game-of-life.js
@@ -15,19 +15,24 @@ export function generateNext(previous) {
     for (let col = 0; col < previous[row].length; col++) {
       const neighbourCount = countNeighbours(row, col, previous)
       //Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-      if (neighbourCount < 2 && previous[row][col] === true) {
+      //Any live cell with more than three live neighbours dies, as if by overpopulation.
+      if (
+        isLiveAndHasFewerThanTwoLiveNeighbours(
+          neighbourCount,
+          previous[row][col]
+        ) ||
+        isOverpopulation(neighbourCount, previous[row][col])
+      ) {
         next[row][col] = false
         //Any live cell with two or three live neighbours lives on to the next generation.
-      } else if (
-        (neighbourCount === 2 || neighbourCount === 3) &&
-        previous[row][col] === true
-      ) {
-        next[row][col] = true
-        //Any live cell with more than three live neighbours dies, as if by overpopulation.
-      } else if (neighbourCount > 3 && previous[row][col] === true) {
-        next[row][col] = false
         //Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.r
-      } else if (neighbourCount === 3 && previous[row][col] === false) {
+      } else if (
+        isLiveAndHasTwoOrThreeLiveNeighbours(
+          neighbourCount,
+          previous[row][col]
+        ) ||
+        isNewborn(neighbourCount, previous[row][col])
+      ) {
         next[row][col] = true
       }
     }
@@ -35,11 +40,30 @@ export function generateNext(previous) {
   return next
 }
 
+function isNewborn(neighbourCount, cell) {
+  return neighbourCount === 3 && cell === false
+}
+
+function isOverpopulation(neighbourCount, cell) {
+  return neighbourCount > 3 && cell === true
+}
+
+function isLiveAndHasTwoOrThreeLiveNeighbours(neighbourCount, cell) {
+  return (neighbourCount === 2 || neighbourCount === 3) && cell === true
+}
+
+function isLiveAndHasFewerThanTwoLiveNeighbours(neighbourCount, cell) {
+  return neighbourCount < 2 && cell === true
+}
+
 function countNeighbours(row, col, previous) {
   let neighbourCount = 0
   for (let x = -1; x < 2; x += 1) {
     for (let y = -1; y < 2; y += 1) {
-      if (!isItself(col, x, y) && isAliveNeighbour(previous, row, col, x, y)) {
+      if (
+        !isItself(col, x, y) &&
+        isAliveNeighbour(previous, row + y, col + x)
+      ) {
         neighbourCount++
       }
     }
@@ -47,11 +71,11 @@ function countNeighbours(row, col, previous) {
   return neighbourCount
 }
 
-function isAliveNeighbour(previous, row, col, x, y) {
-  if (row + y < 0) return false
-  if (col + x < 0) return false
-  if (row + y > previous.length - 1) return false
-  return previous[row + y][col + x] === true
+function isAliveNeighbour(previous, y, x) {
+  if (y < 0) return false
+  if (x < 0) return false
+  if (y > previous.length - 1) return false
+  return previous[y][x] === true
 }
 
 function isItself(col, x, y) {
