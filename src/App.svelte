c81991a19d8cf04978effce85ac83e09be9050e1
Juha Wiberg <juha.wiberg@gmail.com>
2019-12-10 23:20:32 +0200

    Some refactoring...

--- src/App.svelte
+++ src/App.svelte
@@ -5,34 +5,24 @@
 
   export let running = false
   let speed = 0
-  let intervalTime = 2000
-
-  //TODO: remove this
-  let interval = setInterval(() => {
-    if (running) {
-      next = gol.generateNext(previous)
-    }
-  }, intervalTime)
-
-  $: {
-    intervalTime = 2000 - speed
-
-    console.log(intervalTime)
-
-    clearInterval(interval)
-
-    interval = setInterval(() => {
-      if (running) {
-        next = gol.generateNext(previous)
-      }
-    }, intervalTime)
-  }
+  let interval;
 
   const width = 100
   const height = 100
 
   let previous = gol.createEmptyGrid(width, height)
   let next = gol.createEmptyGrid(width, height)
+
+
+  $: {
+    clearInterval(interval)
+
+    interval = setInterval(() => {
+      if (running) {
+        next = gol.generateNext(previous)
+      }
+    }, 2000 - speed)
+  }
 </script>
 
 <ControlPanel bind:running bind:speed />
